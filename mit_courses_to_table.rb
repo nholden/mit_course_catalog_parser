ea69f88ccd2e7e01ec476361e659edd4b3f2117f
Nick Holden <nholden@bu.edu>
2015-05-30 08:31:50 -0400

    Add get_course_title method

--- mit_courses_to_table.rb
+++ mit_courses_to_table.rb
@@ -1,3 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
-course_catalog_page = Nokogiri::HTML(open("http://student.mit.edu/catalog/m16a.html"))
+@page = Nokogiri::HTML(open("http://student.mit.edu/catalog/m16a.html"))
+
+def get_course_title(n)
+  course_titles = []
+  h3_tags = @page.xpath("//h3")
+  h3_tags.each do |h3_tag|
+    h3_tag_text = h3_tag.text.split("\n")
+    course_titles << h3_tag_text[0] if /\d.*$/.match(h3_tag.text[0])
+  end
+  return course_titles[n]
+end
