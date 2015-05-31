00b76c0d71d20b20546bf98d9e138bff1f374a46
Nick Holden <nholden@bu.edu>
2015-05-31 10:40:19 -0400

    Refactor and add methods to scrape course number, title, instructors, and units

--- mit_courses_to_table.rb
+++ mit_courses_to_table.rb
@@ -1,13 +1,21 @@
 require 'nokogiri'
 require 'open-uri'
-@page = Nokogiri::HTML(open("http://student.mit.edu/catalog/m16a.html"))
+page = Nokogiri::HTML(open("http://student.mit.edu/catalog/m16a.html")).to_s
+page.gsub!(/<a name="\d.*$/, "<div class='course'>\n\\0").gsub!(/<!--end-->/, "\\0\n</div>")
+@courses = Nokogiri::HTML(page).xpath("//div[@class='course']")
 
-def get_course_title(n)
-  course_titles = []
-  h3_tags = @page.xpath("//h3")
-  h3_tags.each do |h3_tag|
-    h3_tag_text = h3_tag.text.split("\n")
-    course_titles << h3_tag_text[0] if /\d.*$/.match(h3_tag.text[0])
-  end
-  return course_titles[n]
+def get_num(n)
+  @courses[n].xpath("a")[0].xpath("@name")
+end
+
+def get_title(n)
+  @courses[n].xpath("h3").text.match(/^[\d\.a-zA-Z]* (.*)$/)[1]
+end
+
+def get_instructors(n)
+  @courses[n].xpath("i")[-1].text
+end
+
+def get_units(n)
+  @courses[n].text.match(/^Units: (.*)$/)[1]
 end
