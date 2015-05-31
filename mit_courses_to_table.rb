c2a2f32e1b4e76ca6c8222c25b4d04b1d36dd3ca
Nick Holden <nholden@bu.edu>
2015-05-31 14:39:49 -0400

    Install Sinatra

--- mit_courses_to_table.rb
+++ mit_courses_to_table.rb
@@ -1,146 +1,148 @@
 require 'nokogiri'
 require 'open-uri'
 require 'erb'
+require 'sinatra'
 
-page = Nokogiri::HTML(open("http://student.mit.edu/catalog/m16a.html")).to_s
-page.gsub!(/<a name="\d.*$/, "<div class='course'>\n\\0").gsub!(/<!--end-->/, "\\0\n</div>")
-@courses = Nokogiri::HTML(page).xpath("//div[@class='course']")
-
-def get_num(n)
-  @courses[n].xpath("a")[0].xpath("@name")
-end
-
-def get_title(n)
-  @courses[n].xpath("h3").text.match(/^[\d\.a-zA-Z]* (.*)$/)[1]
-end
-
-def get_instructors(n)
-  @courses[n].xpath("i")[-1].text unless @courses[n].xpath("i")[-1].nil?
-end
-
-def get_units(n)
-  units_match_data = @courses[n].text.match(/^Units: (.*)$/)
-  units_match_data[1] unless units_match_data.nil?
-end
-
-def get_level(n)
-  images = @courses[n].xpath("img//@src").to_s
-  if images.match(/under.gif/)
-    "U"
-  elsif images.match(/grad.gif/)
-    "G"
+get '/' do
+  page = Nokogiri::HTML(open("http://student.mit.edu/catalog/m16a.html")).to_s
+  page.gsub!(/<a name="\d.*$/, "<div class='course'>\n\\0").gsub!(/<!--end-->/, "\\0\n</div>")
+  @courses = Nokogiri::HTML(page).xpath("//div[@class='course']")
+ 
+  def get_num(n)
+    @courses[n].xpath("a")[0].xpath("@name")
   end
-end
 
-def is_spring?(n)
-  images = @courses[n].xpath("img//@src").to_s
-  if images.match(/spring.gif/)
-    true
-  else
-    false
+  def get_title(n)
+    @courses[n].xpath("h3").text.match(/^[\d\.a-zA-Z]* (.*)$/)[1]
   end
-end
 
-def is_fall?(n)
-  images = @courses[n].xpath("img//@src").to_s
-  if images.match(/fall.gif/)
-    true
-  else
-    false
+  def get_instructors(n)
+    @courses[n].xpath("i")[-1].text unless @courses[n].xpath("i")[-1].nil?
   end
-end
 
-def is_iap?(n)
-  images = @courses[n].xpath("img//@src").to_s
-  if images.match(/iap.gif/)
-    true
-  else
-    false
+  def get_units(n)
+    units_match_data = @courses[n].text.match(/^Units: (.*)$/)
+    units_match_data[1] unless units_match_data.nil?
   end
-end
 
-def is_summer?(n)
-  images = @courses[n].xpath("img//@src").to_s
-  if images.match(/summer.gif/)
-    true
-  else
-    false
+  def get_level(n)
+    images = @courses[n].xpath("img//@src").to_s
+    if images.match(/under.gif/)
+      "U"
+    elsif images.match(/grad.gif/)
+      "G"
+    end
   end
-end
 
-def get_lecture_times(n)
-  lecture_match_data = @courses[n].text.match(/Lecture: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
-  lecture_match_data[1] unless lecture_match_data.nil?
-end
-
-def get_lecture_room(n)
-  lecture_match_data = @courses[n].text.match(/Lecture: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
-  lecture_match_data[2] unless lecture_match_data.nil?
-end 
-
-def is_lecture?(n)
-  if get_lecture_times(n).nil? 
-    false
-  else
-    true
+  def is_spring?(n)
+    images = @courses[n].xpath("img//@src").to_s
+    if images.match(/spring.gif/)
+      true
+    else
+      false
+    end
   end
-end
 
-def get_lab_times(n)
-  two_labs_match_data = @courses[n].text.match(/Lab: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\) or ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
-  if two_labs_match_data.nil?
-    lab_match_data = @courses[n].text.match(/Lab: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
-    [lab_match_data[1]] unless lab_match_data.nil?
-  else
-    [two_labs_match_data[1], two_labs_match_data[3]]
+  def is_fall?(n)
+    images = @courses[n].xpath("img//@src").to_s
+    if images.match(/fall.gif/)
+      true
+    else
+      false
+    end
   end
-end
 
-def get_lab_room(n)
-  two_labs_match_data = @courses[n].text.match(/Lab: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\) or ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
-  if two_labs_match_data.nil?
-    lab_match_data = @courses[n].text.match(/Lab: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
-    [lab_match_data[2]] unless lab_match_data.nil?
-  else
-    [two_labs_match_data[2], two_labs_match_data[4]]
+  def is_iap?(n)
+    images = @courses[n].xpath("img//@src").to_s
+    if images.match(/iap.gif/)
+      true
+    else
+      false
+    end
   end
-end
 
-def is_lab?(n)
-  if get_lab_times(n).nil? 
-    false
-  else
-    true
+  def is_summer?(n)
+    images = @courses[n].xpath("img//@src").to_s
+    if images.match(/summer.gif/)
+      true
+    else
+      false
+    end
   end
-end
 
-def get_recitation_times(n)
-  two_recitations_match_data = @courses[n].text.match(/Recitation: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\) or ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
-  if two_recitations_match_data.nil?
-    recitation_match_data = @courses[n].text.match(/Recitation: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
-    [recitation_match_data[1]] unless recitation_match_data.nil?
-  else
-    [two_recitations_match_data[1], two_recitations_match_data[3]]
+  def get_lecture_times(n)
+    lecture_match_data = @courses[n].text.match(/Lecture: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
+    lecture_match_data[1] unless lecture_match_data.nil?
   end
-end
 
-def get_recitation_room(n)
-  two_recitations_match_data = @courses[n].text.match(/Recitation: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\) or ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
-  if two_recitations_match_data.nil?
-    recitation_match_data = @courses[n].text.match(/Recitation: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
-    [recitation_match_data[2]] unless recitation_match_data.nil?
-  else
-    [two_recitations_match_data[2], two_recitations_match_data[4]]
+  def get_lecture_room(n)
+    lecture_match_data = @courses[n].text.match(/Lecture: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
+    lecture_match_data[2] unless lecture_match_data.nil?
+  end 
+
+  def is_lecture?(n)
+    if get_lecture_times(n).nil? 
+      false
+    else
+      true
+    end
   end
-end
 
-def is_recitation?(n)
-  if get_recitation_times(n).nil? 
-    false
-  else
-    true
+  def get_lab_times(n)
+    two_labs_match_data = @courses[n].text.match(/Lab: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\) or ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
+    if two_labs_match_data.nil?
+      lab_match_data = @courses[n].text.match(/Lab: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
+      [lab_match_data[1]] unless lab_match_data.nil?
+    else
+      [two_labs_match_data[1], two_labs_match_data[3]]
+    end
   end
-end
 
-template = File.read('template.html.erb')
-File.open('table.html', 'w+') { |file| file.write(ERB.new(template).result) }
+  def get_lab_room(n)
+    two_labs_match_data = @courses[n].text.match(/Lab: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\) or ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
+    if two_labs_match_data.nil?
+      lab_match_data = @courses[n].text.match(/Lab: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
+      [lab_match_data[2]] unless lab_match_data.nil?
+    else
+      [two_labs_match_data[2], two_labs_match_data[4]]
+    end
+  end
+
+  def is_lab?(n)
+    if get_lab_times(n).nil? 
+      false
+    else
+      true
+    end
+  end
+
+  def get_recitation_times(n)
+    two_recitations_match_data = @courses[n].text.match(/Recitation: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\) or ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
+    if two_recitations_match_data.nil?
+      recitation_match_data = @courses[n].text.match(/Recitation: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
+      [recitation_match_data[1]] unless recitation_match_data.nil?
+    else
+      [two_recitations_match_data[1], two_recitations_match_data[3]]
+    end
+  end
+
+  def get_recitation_room(n)
+    two_recitations_match_data = @courses[n].text.match(/Recitation: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\) or ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
+    if two_recitations_match_data.nil?
+      recitation_match_data = @courses[n].text.match(/Recitation: ([a-zA-Z\d]*) \(([a-zA-Z\d-]*)\)/)
+      [recitation_match_data[2]] unless recitation_match_data.nil?
+    else
+      [two_recitations_match_data[2], two_recitations_match_data[4]]
+    end
+  end
+
+  def is_recitation?(n)
+    if get_recitation_times(n).nil? 
+      false
+    else
+      true
+    end
+  end
+
+  erb :template
+end
